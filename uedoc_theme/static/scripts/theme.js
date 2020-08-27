cdc674d508256e1064fe3fed65263f179f9534bf
Darkimage <luc-af@live.it>
2020-08-27 21:22:41 +0200

    Working on theming system

--- uedoc_theme/static/scripts/theme.js
+++ uedoc_theme/static/scripts/theme.js
@@ -1,13 +1,50 @@
-function generateTheme(css) {
-	var cssObj = JSON.parse(css);
-	var clr_background = chroma(cssObj['background']).hsl();
-	document.documentElement.style.setProperty('--clr-background-h', clr_background[0]);
-	document.documentElement.style.setProperty('--clr-background-s', clr_background[1]*100+'%');
-	document.documentElement.style.setProperty('--clr-background-l',clr_background[2]*100+'%');
-	// document.documentElement.style.setProperty('--clr-background-hsl', clr_background[0] + ',' + clr_background[1]*100+'%' + ',' + clr_background[2]*100+'%');
-}
+
+var uedoc_theme = {
+	getSafeValue: (nmb) => {
+		return (isNaN(nmb)) ? 0 : nmb;
+	},
+	
+	convertToPercent: (clr) => {
+		return `${clr * 100}%`;
+	},
+	
+	getColor: (clr, type) => {
+		if ($.isArray(clr)) {
+			return clr.map(function (clrElem, index) {
+				console.log(clrElem)
+				switch (type) {
+					case 'hsl':
+						if (index != 0) {
+							return uedoc_theme.convertToPercent(uedoc_theme.getSafeValue(clrElem));
+						}
+						return uedoc_theme.getSafeValue(clrElem);
+					default:
+						return uedoc_theme.getSafeValue(clrElem);
+				}
+			});
+		} else {
+			return uedoc_theme.getSafeValue(clr)
+		}
+	},
+
+	setThemeColor: (key, clr) => {
+		var color = uedoc_theme.getColor(chroma(clr).hsl(), 'hsl');
+		$(':root').css(`${key}-h`, color[0]);
+		$(':root').css(`${key}-s`, color[1]);
+		$(':root').css(`${key}-l`, color[2]);
+	},
+
+	generateTheme: (css) => {
+		var cssObj = JSON.parse(css);
+		for (const color in cssObj) {
+			if (cssObj.hasOwnProperty(color)) {
+				const theme_color = cssObj[color];
+				uedoc_theme.setThemeColor(`--clr-${color}`, theme_color);
+			}
+		}
+	}
+};
 
 $(window).on('load', function () {
-	console.log($('#uedoc_theme').html())
-	generateTheme($('#uedoc_theme').html())
+	uedoc_theme.generateTheme($('#uedoc_theme').html());
 })
