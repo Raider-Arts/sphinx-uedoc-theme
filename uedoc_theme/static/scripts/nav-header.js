fb9f1008565533e8ebd57dcc99dc88aadeab691d
Darkimage <luc-af@live.it>
2020-08-29 00:32:41 +0200

    Added animation to header nav-menu

--- uedoc_theme/static/scripts/nav-header.js
+++ uedoc_theme/static/scripts/nav-header.js
@@ -1,6 +1,15 @@
 
+var navopen = false
+
 $(window).on('load', function () {
 	$('#head div.hamburger-nav').on('click', function () {
-		console.log('ckic')
+		if (navopen) {
+			$('body').removeClass('scroll-block');
+			$('div#nav-menu').removeClass('open-nav');
+		} else {
+			$('body').addClass('scroll-block')
+			$('div#nav-menu').addClass('open-nav');
+		}
+		navopen = !navopen;
 	})
 })
