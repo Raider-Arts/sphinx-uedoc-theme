791263d4cecb560a809fb6121609472545f4116b
Darkimage <luc-af@live.it>
2020-09-05 23:29:57 +0200

    Fixed nav-menu not resetting when window resize

--- src/scripts/nav-header.js
+++ src/scripts/nav-header.js
@@ -1,14 +1,38 @@
-var navopen = false
+import $ from 'jquery';
+
+var navopen = true
+
+/**
+ * Change the animate state of the nav-menu, triggering the css animation
+ * @param {bool} state 
+ */
+export function animateNavMenu(state = false){
+	if (state) {
+		$('body').addClass('scroll-block')
+		$('div#nav-menu').addClass('open-nav');
+	} else {
+		$('body').removeClass('scroll-block');
+		$('div#nav-menu').removeClass('open-nav');
+	}
+}
+
+/**
+ * Toggle the aniamtion each time the function get executed
+ */
+export function toggleNavMenu() {
+	animateNavMenu(navopen);
+	navopen = !navopen;
+}
+
+/**
+ * Register Windows Callbacks
+ */
 
 $(window).on('load', function () {
-	$('#head div.hamburger-nav').on('click', function () {
-		if (navopen) {
-			$('body').removeClass('scroll-block');
-			$('div#nav-menu').removeClass('open-nav');
-		} else {
-			$('body').addClass('scroll-block')
-			$('div#nav-menu').addClass('open-nav');
-		}
-		navopen = !navopen;
-	})
+	$('#head div.hamburger-nav').on('click', toggleNavMenu)
+})
+
+$(window).on('resize', () => {
+	animateNavMenu(false)
+	navopen = true;
 })
