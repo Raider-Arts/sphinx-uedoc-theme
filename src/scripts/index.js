8f5f5ce6e1a648f358e0b7d4a3c7c52fc1be71df
Darkimage <luc-af@live.it>
2020-09-08 10:24:53 +0200

    Added svg for fontaweasome

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,11 @@ import './sidebar';
 import './splitter';
 import './filter-pages';
 import './nav-header';
+import '@fortawesome/fontawesome-free/css/all.css';
+import { library, dom } from'@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, fab, far);
+dom.watch(); 
