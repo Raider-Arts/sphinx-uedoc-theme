d1465d453215d4950a9c9e266cf8f7c514d1a4e9
Darkimage <luc-af@live.it>
2020-09-08 12:23:55 +0200

    Added fontawesome tree-shaking

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,11 +4,12 @@ import './sidebar';
 import './splitter';
 import './filter-pages';
 import './nav-header';
-import '@fortawesome/fontawesome-free/css/all.css';
-import { library, dom } from'@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { library, dom, config } from'@fortawesome/fontawesome-svg-core';
+import { faLink, faSearch, faRss } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas, fab, far);
+config.searchPseudoElements = true;
+config.observeMutations = true;
+config.autoReplaceSvg = true;
+library.add(faLink, faSearch, faRss, fab);
 dom.watch(); 
