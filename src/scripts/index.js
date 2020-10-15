1bc1ecde2e7f05a71a91251b5f103729497cc21e
Darkimage <luc-af@live.it>
2020-10-15 13:18:49 +0200

    Added icons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,14 +6,14 @@ import './filter-pages';
 import './nav-header';
 import logo from '../images/sphinx-ue-logo.svg';
 import { library, dom, config } from'@fortawesome/fontawesome-svg-core';
-import { faLink, faSearch, faRss, faBars, faAngleLeft, faAngleRight, faChevronUp, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { faLink, faSearch, faRss, faBars, faAngleLeft, faAngleRight, faChevronUp, faFolderOpen, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { loadSvgFromString } from './utilities'
 
 config.searchPseudoElements = true;
 config.observeMutations = true;
 config.autoReplaceSvg = true;
-library.add(faLink, faSearch, faRss, faBars, fab, faAngleLeft, faAngleRight, faChevronUp, faFolderOpen);
+library.add(faLink, faSearch, faRss, faBars, fab, faAngleLeft, faAngleRight, faChevronUp, faFolderOpen, faPlus, faMinus);
 dom.watch(); 
 
 $(window).on('load', function () {
